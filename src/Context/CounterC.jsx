f2436579c3a5fc37c22d3d89ce3afb37b82be001
rohitprocode <rohit990797@gmail.com>
2024-01-03 21:51:48 +0530

    null

--- src/Context/CounterC.jsx
+++ src/Context/CounterC.jsx
@@ -6,7 +6,7 @@ export const CounterContext = createContext(null)
 
 export const CounterProvider = (props) =>{
 
-    const [count,setCount] = useState(5)
+    const [count,setCount] = useState(null)
     return (
         <CounterContext.Provider value={{count,setCount,name:'Rohit'}} >
             {props.children}
